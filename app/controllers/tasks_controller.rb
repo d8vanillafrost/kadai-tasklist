00a1b14b830e327ee05dbbec7acbf5ef85b7cecb
EC2 Default User <ec2-user@ip-172-31-22-108.us-east-2.compute.internal>
2018-04-04 14:56:45 +0000

    bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -9,12 +9,11 @@ class TasksController < ApplicationController
   end
 
   def new
-     @task = Task.new
+    @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
-
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
       redirect_to @task
@@ -29,8 +28,6 @@ class TasksController < ApplicationController
   end
 
   def update
-
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -50,10 +46,10 @@ class TasksController < ApplicationController
   
   private
     def set_task
-    @task = Task.find(params[:id])
+      @task = Task.find(params[:id])
     end
   
    def task_params
-    params.require(:task).permit(:content,:status)
+     params.require(:task).permit(:content,:status)
    end
 end
